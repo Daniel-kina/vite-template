d8e5cc6ce45cc551663da671c4c752d9c8bfb1b8
Daniel <nisaki2003@gmail.com>
2026-06-21 16:34:53 +0200

    added svgr plugin & prettier config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr"; //
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
-    globals: true,           // Erlaubt describe, test, expect ohne Import
-    environment: 'jsdom',    // Simuliert den Browser
-    setupFiles: './src/tests/setup.js', // Lädt globale Test-Einstellungen
+    globals: true, // Erlaubt describe, test, expect ohne Import
+    environment: "jsdom", // Simuliert den Browser
+    setupFiles: "./src/tests/setup.js", // Lädt globale Test-Einstellungen
   },
-})
+});
